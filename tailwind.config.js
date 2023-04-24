7172854c8eddaaeb7400e4e3668d7c02702986fa
taiphan2288 <67281521+taiphan2288@users.noreply.github.com>
2023-04-25 01:24:00 +0700

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-const plugin = require("tailwindcss/plugin");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
@@ -92,10 +91,5 @@ module.exports = {
       ],
     },
   },
-  plugins: [
-    require("@tailwindcss/line-clamp"),
-    plugin(function ({ addVariant }) {
-      addVariant("not-last-child", "&:not(:last-child)");
-    }),
-  ],
+  plugins: [],
 };
