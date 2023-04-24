ab1e7c3511b495ef9aaf9484b0509aeb9d680702
taiphan2288 <67281521+taiphan2288@users.noreply.github.com>
2023-04-25 01:26:30 +0700

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./Components/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
     extend: {
