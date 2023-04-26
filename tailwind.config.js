a7963d33be7c64eaa9af9d3f8462f122ad94f379
taiphan2288 <67281521+taiphan2288@users.noreply.github.com>
2023-04-26 16:40:20 +0700

    fix shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -52,7 +52,8 @@ module.exports = {
       boxShadow: {
         "3xl": "0 15px 15px 1px rgba(80,230,217, 0.4)",
         cardShadow: "0 2px 10px rgba(0, 0, 0, 0.2)",
-        "box-shadow-dark": "-23px 23px 62px -17px rgba(0,0,0,0.72)",
+        "box-shadow-dark":
+          "rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px",
         "box-shadow-white": "-23px 23px 62px -17px rgba(255,255,255,0.72)",
       },
 
